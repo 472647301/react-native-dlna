6b69d45c00f740445511fde3193196147ceb2b91
秃尾巴的猫 <472647301@qq.com>
2021-10-09 14:36:39 +0800

    Update DLNAService.java

--- android/src/main/java/com/plutinosoft/service/DLNAService.java
+++ android/src/main/java/com/plutinosoft/service/DLNAService.java
@@ -1,11 +1,19 @@
 package com.plutinosoft.service;
 
+import android.annotation.SuppressLint;
 import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.os.IBinder;
+
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
 
 import com.plutinosoft.event.NativeAsyncEvent;
 import com.plutinosoft.instance.ServerInstance;
@@ -33,11 +41,33 @@ public class DLNAService extends Service {
         return null;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @SuppressLint("ObsoleteSdkInt")
     @Override
     public void onCreate() {
         super.onCreate();
         acquireMulticastLock();
         EventBus.getDefault().register(this);
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            startForeground(1, new Notification());
+        } else {
+            String NOTIFICATION_CHANNEL_ID = "com.plutinosoft.service";
+            String channelName = "DLNA Background Service";
+            NotificationChannel chan = new NotificationChannel(NOTIFICATION_CHANNEL_ID, channelName, NotificationManager.IMPORTANCE_NONE);
+            chan.setLightColor(Color.BLUE);
+            chan.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+            NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            assert manager != null;
+            manager.createNotificationChannel(chan);
+
+            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+            Notification notification = notificationBuilder.setOngoing(true)
+                .setContentTitle("App is running in background")
+                .setPriority(NotificationManager.IMPORTANCE_MIN)
+                .setCategory(Notification.CATEGORY_SERVICE)
+                .build();
+            startForeground(2, notification);
+        }
     }
 
     private void acquireMulticastLock() {
